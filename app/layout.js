1e1c9b45f1b4bd7c4b3d08192228453c2ddd9b52
konwarsantanu687-ui <konwarsantanu687@gmail.com>
2026-02-09 11:54:43 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "HamaraMarket • Founder",
+  description: "Founder profile of HamaraMarket"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
